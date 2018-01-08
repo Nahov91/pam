830ca43b18e0faa96c5e268266e96031f43df7ca
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-01-08 23:15:44 +0100

    Added variables of values, implemented makeGrid, which creates the canvas for PAM.Implemented color selector as well. Those functions are only operating with their default values for now.

--- designs.js
+++ designs.js
@@ -2,9 +2,24 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var gridHeight = document.getElementById('inputHeight').value;
+var gridWidth = document.getElementById('inputWidth').value;
+var color = document.getElementById('colorPicker').value;
 
 function makeGrid() {
-
-// Your code goes here!
-
+  for (var i = 0; i < gridHeight; i++) {
+    $('#pixel_canvas').append($('<tr></tr>'))
+  }
+    for (var i = 0; i < gridWidth; i++) {
+      $('tr').append($('<td></td>'))
+    }
 }
+
+$('submit').on('click', makeGrid());
+
+$('td').on('click', function() {
+  $(this).bgColor = color;
+});
+console.log(gridWidth);
+console.log(gridHeight);
+console.log(color);
