a5ff683f5a15f0e7eecfc376c1062c7f8fc8d59b
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-01-10 00:04:24 +0100

    Grid size selection is now fully functional.

--- designs.js
+++ designs.js
@@ -2,24 +2,37 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-var gridHeight = document.getElementById('inputHeight').value;
-var gridWidth = document.getElementById('inputWidth').value;
+
 var color = document.getElementById('colorPicker').value;
+var subButton = document.getElementById('canvasSubmit');
+var canvas = document.getElementById('pixel_canvas');
 
 function makeGrid() {
+  $('#pixel_canvas').children().remove();
+  var gridHeight = $('#inputHeight').val();
+  var gridWidth = $('#inputWidth').val();
+
   for (var i = 0; i < gridHeight; i++) {
-    $('#pixel_canvas').append($('<tr></tr>'))
+    $('#pixel_canvas').append('<tr></tr>');
   }
-    for (var i = 0; i < gridWidth; i++) {
-      $('tr').append($('<td></td>'))
-    }
+
+  for (var j = 0; j < gridWidth; j++) {
+    $('tr').each(function () {
+      $(this).append('<td></td>');
+    });
+  }
+
+  console.log('width= ' + gridWidth);
+  console.log('Height= ' + gridHeight);
 }
 
-$('submit').on('click', makeGrid());
+$('#canvasSubmit').click(function (event) {
+  event.preventDefault();
+  makeGrid();
+});
 
-$('td').on('click', function() {
+$('td').on('click', function () {
   $(this).bgColor = color;
 });
-console.log(gridWidth);
-console.log(gridHeight);
+
 console.log(color);
